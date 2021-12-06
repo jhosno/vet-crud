2895dd0744fdce7fd2dc57cc7d395b07d4d992a0
jhosno <jhosnoirlit@gmail.com>
2021-12-06 18:09:44 -0400

    dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   //Remover estilos que no estemos utilizando en los archivos index.html 
   //y en todos (**) los directorios hijos de /src/ con extensión *.{jsx}
-  purge: ["./index.html", "./src/**/*.{jsx}"],
+  purge: ["./index.html", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
